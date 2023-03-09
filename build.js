b2df1d96aa34c9e28b504bbdded0bb1d3e54e3c6
Ridho Yusuf <ridho@Ridhos-MacBook-Air.local>
2023-03-10 02:44:53 +1100

    fixed carousel

---
+++ build.js
@@ -0,0 +1,33 @@
+// Course notes links
+
+document.querySelector('.cs1511').addEventListener('click', () => {
+    open('https://drive.google.com/u/0/uc?id=15BrgL7cfLTBjQjOa2RluiHC3nwYjpi0C&export=download')
+})
+
+document.querySelector('.cs1521').addEventListener('click', () => {
+    open('https://drive.google.com/u/0/uc?id=1pQJJ74G8B7ZH7wUuM75NNU3xStzlx39Q&export=download')
+})
+
+document.querySelector('.cs1531').addEventListener('click', () => {
+    open('https://drive.google.com/u/0/uc?id=1ie4rxusR4JtoAngkuSyaLttpZn796DQC&export=download')
+})
+
+document.querySelector('.cs2511').addEventListener('click', () => {
+    open('https://drive.google.com/u/0/uc?id=1sVDtycXCAKrjKD-_oaEmL4jwb3lNIiBw&export=download')
+})
+
+document.querySelector('.cs2521').addEventListener('click', () => {
+    open('https://drive.google.com/u/0/uc?id=1wAnUr9pTg0HLGW_hhWkRDNlknjdNwmwE&export=download')
+})
+
+document.querySelector('.cs2041').addEventListener('click', () => {
+    open('https://ridho-y.github.io/comp2041/')
+})
+
+document.querySelector('.cs3311').addEventListener('click', () => {
+    open('https://ridho-y.github.io/comp3311/')
+})
+
+document.querySelector('.cs6080').addEventListener('click', () => {
+    open('https://ridho-y.github.io/comp6080/')
+})
