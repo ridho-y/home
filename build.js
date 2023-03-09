13ef5535f8cb8b5712bab81dac805561b8e21a6a
Ridho Yusuf <ridho@Ridhos-MacBook-Air.local>
2023-03-10 02:50:28 +1100

    fixed carousel again

--- build.js
+++ build.js
@@ -1,3 +1,40 @@
+// Carousel for course notes
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    speed: 1000,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    allowTouchMove: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+});
+
 // Course notes links
 
 document.querySelector('.cs1511').addEventListener('click', () => {
